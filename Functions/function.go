3c2298306ffc76ffe5c34176317c183600e19d04
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-08-30 23:07:48 +0530

    Functions

---
+++ Functions/function.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func add(Num1 int, Num2 int) int {
+	sum := Num1 + Num2
+	return sum
+}
+
+func cal(Num3 int, Num4 int) (int, int) {
+	Mul := Num3 * Num4
+	Mod := Num3 % Num4
+	return Mul, Mod
+}
+
+func main() {
+	Add := add(21, 41)
+	fmt.Println(Add)
+	Res1, Res2 := cal(31, 51)
+	fmt.Println(Res1, Res2)
+}

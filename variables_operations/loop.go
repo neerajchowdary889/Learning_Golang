7001fcd7cbe6c04264762a2ac9751f7fc8a988ac
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-08-30 22:32:06 +0530

    Initial Commit

---
+++ variables_operations/loop.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func Forloops() {
+
+	for i := 0; i <= 5; i++ {
+		fmt.Println("Hello", i)
+	}
+}
--- variables_operations/variables.go
+++ variables_operations/variables.go
@@ -3,8 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	variables()
+	operations()
+	Forloops()
+}
+
+func variables() {
 	var Name = "Neeraj"
 	var Age = 18
 	fmt.Println("Name: " + Name)
 	fmt.Println("Age: ", Age)
+	fmt.Println("_________________end of Variables")
+}
+
+func operations() {
+	num1, num2, num3 := 21, 31, 41
+	const num = 15
+	sum := num1 + num2 + num3
+	sub := sum - num
+	mult := num1 * num2 * num3
+	division := sum / num
+	Mod := sum % num
+	fmt.Println("Sum", sum)
+	fmt.Println("sub", sub)
+	fmt.Println("Multiply", mult)
+	fmt.Println("Division", division)
+	fmt.Println("Mod", Mod)
+	fmt.Println("_________________end of Operations")
+
 }

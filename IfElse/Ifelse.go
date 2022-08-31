d71e7795b00a0846988514d85281b49af728ee84
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-08-31 18:02:17 +0530

    ifelse

--- IfElse/Ifelse.go
+++ IfElse/Ifelse.go
@@ -7,8 +7,10 @@ func main() {
 	fmt.Print("Type out your input here: ")
 	var inp int
 	fmt.Scan(&inp)
-	if inp <= 5 {
+	if inp < 5 {
 		fmt.Println("Hello")
+	} else if inp == 5 {
+		fmt.Println("Stay here")
 	} else {
 		fmt.Println("bye")
 		fmt.Println("The End...")

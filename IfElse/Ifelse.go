16095e5421cb51b794db1da5ae782e16e906f38c
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-08-31 17:59:58 +0530

    switch statement

--- IfElse/Ifelse.go
+++ IfElse/Ifelse.go
@@ -13,4 +13,5 @@ func main() {
 		fmt.Println("bye")
 		fmt.Println("The End...")
 	}
+	switches()
 }
---
+++ IfElse/switch.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func switches() {
+	fmt.Println("Enter between 1 to 4")
+	var inp int
+	fmt.Scan(&inp)
+
+	switch inp {
+	case 1:
+		fmt.Println(1, "Switch case")
+	case 2:
+		fmt.Println(2, "Switch case")
+	case 3:
+		fmt.Println(3, "Switch case")
+	case 4:
+		fmt.Println(4, "Switch case")
+	default:
+		fmt.Println("Wrong Input")
+	}
+
+}
